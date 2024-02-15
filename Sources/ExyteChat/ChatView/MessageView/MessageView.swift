44ea68ac315042ba2eddada35ba48e267c8e5653
brian <mr.brianlane@gmail.com>
2024-02-15 07:50:55 -0500

    Update MessageView.swift

--- Sources/ExyteChat/ChatView/MessageView/MessageView.swift
+++ Sources/ExyteChat/ChatView/MessageView/MessageView.swift
@@ -33,7 +33,7 @@ struct MessageView: View {
     static let horizontalStatusPadding: CGFloat = 8
     static let horizontalBubblePadding: CGFloat = 70
 
-    let font = UIFont.systemFont(ofSize: 15)
+    let font = Font.system(.body)
 
     enum DateArrangment {
         case hstack, vstack, overlay
